3812c3265adeb1bd49504c652c9ed6d6defd6dea
luoshaokang <luoshaokang@jd.com>
2022-02-21 23:01:36 +0800

    feat: init theme config

--- src/popup/views/newVersion/index.jsx
+++ src/popup/views/newVersion/index.jsx
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact'
+import { Button } from '@mui/material'
 import styles from './index.scss'
 export default class NewVersion extends Component {
   render() {
@@ -8,6 +9,9 @@ export default class NewVersion extends Component {
         <div className='version'>123123</div>
         <div className='test'>test</div>
         <p>This is a simple example of a popup.</p>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
       </div >
     )
   }
